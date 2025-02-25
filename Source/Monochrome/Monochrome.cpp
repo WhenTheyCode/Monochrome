0e9c80cf15b2c8af1a00abe053d342ac37a1ecb8
Tomás <162116803+WhenTheyCode@users.noreply.github.com>
2025-02-25 12:28:25 +0000

    more level

---
+++ Source/Monochrome/Monochrome.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Monochrome.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Monochrome, "Monochrome" );
